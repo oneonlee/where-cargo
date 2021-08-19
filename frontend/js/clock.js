cb6de75bea72241f39a8d079fe161f8de2c2af48
이동건 <peter8739@gmail.com>
2021-08-20 07:42:30 +0900

    Revert "Merge branch 'main' into 'master'"
    This reverts merge request !8

--- frontend/js/clock.js
+++ frontend/js/clock.js
@@ -1,10 +1,11 @@
-const clockTitle1 = document.getElementById("js-clock1");
-const clockTitle2 = document.getElementById("js-clock2");
+const clockTitle = document.querySelector("time");
 
-const date = new Date();
-const minutes = date.getMinutes();
-const hours = date.getHours();
-clockTitle1.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
-clockTitle2.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
+function getTime() {
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    // const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
+}
 
-
+getTime();
